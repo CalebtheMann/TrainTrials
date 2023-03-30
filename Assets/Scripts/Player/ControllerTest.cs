f78a0918a4fe9e948d8dafc72755573f92a112b4
EeveeonGamer27 <101752853+EeveeonGamer27@users.noreply.github.com>
2023-03-30 16:15:00 -0500

    Smol changes

--- Assets/Scripts/Player/ControllerTest.cs
+++ Assets/Scripts/Player/ControllerTest.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ControllerTest : MonoBehaviour
 {
