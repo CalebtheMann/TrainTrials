d04daa6445626b299ffce2b8111f818554a04c82
Caleb Purnell <cpurnell@mail.bradley.edu>
2023-04-19 20:49:08 -0500

    BIGDAYBIGDAYBIGDAY

---
+++ Assets/Scripts/General/VictoryScript.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class VictoryScript : MonoBehaviour
+{
+    GameController gameController;
+    /// private Menus input;
+    InputActionAsset inputAsset;
+    InputActionMap inputMap;
+    InputAction play;
+
+    private void Awake()
+    {
+        inputAsset = this.GetComponent<PlayerInput>().actions;
+        inputMap = inputAsset.FindActionMap("Menu");
+        play = inputMap.FindAction("PlayGame");
+        play.performed += ctx => StartGame();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        if (GameObject.Find("Eeveeon") != null)
+        {
+            Destroy(GameObject.Find("Eeveeon"));
+        }
+        gameController = GetComponent<GameController>();
+    }
+
+    public void FixedUpdate()
+    {
+
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene("The Credits");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
